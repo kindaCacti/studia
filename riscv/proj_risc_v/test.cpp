531c2a43ab0cc6384bc6d27e78186ad0d904bf4d
mwawrzy2 <53042410+kindaCacti@users.noreply.github.com>
2024-04-18 20:11:32 +0200

    changed cpp to be easier to turn into asm

--- riscv/proj_risc_v/test.cpp
+++ riscv/proj_risc_v/test.cpp
@@ -1,60 +1,66 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
-struct Pixel{
-    int red, green, blue;
+#define width 320
+#define height 240
 
-    Pixel(int r, int g, int b): red(r), green(g), blue(b){
-    }
+//path buffer for file in asm
+string path = "./example_markers.bmp";
+//only one buffer in asm
+char* rde;
+unsigned char* rd;
+//can be saved in register
+int startingPosition;
 
-    string stringify(){
-        return "{"+to_string(red)+","+to_string(green)+","+to_string(blue)+"}";
-    }
+//instead of pixels use unsigned int int as a pixel
+unsigned int getPixelFromData(int x, int y){
+    int offset = (x + (height - 1 - y) * width) * 3 + startingPosition;
 
-    bool operator==(Pixel& left){
-        if(left.red == red and left.green == green and left.blue == blue){
-            return true;
-        }
-        return false;
-    }
-
-    bool operator!=(Pixel& left){
-        return !((*this) == left);
-    }
-};
-
-vector<unsigned char> readData;
-vector<vector<Pixel>> pixels;
-int width = 320;
-int height = 240;
+    unsigned int pixel = rd[offset + 2];
+    pixel <<= 8;
+    pixel |= rd[offset + 1];
+    pixel <<= 8;
+    pixel |= rd[offset];
+    //cout<<pixel<<" : "<<(int)rd[offset + 2]<<", "<<(int)rd[offset + 1]<<", "<<(int)rd[offset + 2]<<endl;
+    return pixel;
+}
 
 bool checkMarker(int sx, int sy){
-    Pixel markerColor(0,0,0);
-    if(pixels[sy][sx] != markerColor) return false;
+    unsigned int markerColor = 0;
+
+    if(getPixelFromData(sx, sy) != markerColor) return false;
 
     int hgt = 0;
     int lhgt = 0;
     int bhgt = 0;
-    int n = pixels.size();
+    int n = height;
     int cx = sx;
 
     bool hasStepDown = false;
-    if(sx-1 >= 0 and pixels[sy][cx-1] == markerColor) return false;
-    if(sy-1 >= 0 and pixels[sy-1][cx] == markerColor) return false;
+    if(sx-1 >= 0 and getPixelFromData(sx-1, sy) == markerColor) return false;
+    if(sy+1 < height and getPixelFromData(sx, sy-1) == markerColor) return false;
+
+    unsigned int currentPixel;
 
     while(true){
         hgt = 0;
         if(cx == width) break; //don't go outside the image
-        if(pixels[sy][cx] != markerColor) break; //are we still on the marker?
 
-        for(int i = sy; i<pixels.size(); i++){
-            if(pixels[i][cx] != markerColor) break;
+        currentPixel = getPixelFromData(cx, sy);
+
+        if(currentPixel != markerColor) break; //are we still on the marker?
+
+        for(int i = sy; i<n; i++){
+            currentPixel = getPixelFromData(cx, i);
+            if(currentPixel != markerColor) break;
             hgt++;
         }
 
         if(lhgt != 0 and hgt != lhgt){
             if(hasStepDown) return false;
+            if(hgt > lhgt) return false;
             hasStepDown = true;
             bhgt = lhgt;
         }
@@ -65,59 +71,37 @@ bool checkMarker(int sx, int sy){
     
     int wdt = cx - sx;
     bool out = (((wdt == bhgt/2)|(wdt == (bhgt+1)/2))&hasStepDown);
-    //if(out){
-    //    cout<<sx<<"x"<<sy<<":haha "<<wdt<<" : "<<bhgt<<endl;
-    //}
+
     return out;
 }
 
 int main(){
-    ifstream fin("example_markers.bmp");
+    ifstream fin(path);
     int siz = 54 + width * height * 3;
-    char* rd = new char[siz];
-    //while(true){
-    //    if(fin.eof()) break;
-    //    fin>>a;
-    //    readData.push_back(a);
-    //}
-    fin.get(rd, siz);
+    rde = new char[siz];
+    rd = new unsigned char[siz];
+    fin.get(rde, siz);
     fin.close();
 
     for(int i = 0; i<siz; i++){
-        readData.push_back(rd[i]);
+        rd[i] = (unsigned char)rde[i];
     }
-    int startingPosition = readData[10];
+
+   if(rd[0] != 'B' or rd[1] != 'M'){
+       cout<<"wrong file format!";
+       return 0;
+   }
+
+    startingPosition = rd[10];
+
+    getPixelFromData(1, 1);
 
     cout<<startingPosition<<endl;
-    cout<<readData.size()<<endl;
 
-    //width 320px, height 240 px,
-    int moved = 0;
-    for(int i = startingPosition; i<readData.size();){
-        moved = 0;
-        pixels.push_back({});
-        for(int j = i, k = 0; k<width; j+=3, k++){
-            pixels.back().push_back(Pixel(readData[j+2], readData[j+1], readData[j]));
-            moved += 3;
-            i = j+3;
-        }
-        if(moved % 4){
-            i += (4-(moved%4));
-        }
-    }
-
-    reverse(pixels.begin(), pixels.end());
-
-
-    cout<<pixels.back()[0].stringify()<<endl;
-    cout<<pixels.size()<<"x"<<pixels[0].size()<<endl;
-
-    for(int i = 0; i<pixels.size(); i++){
-        for(int j = 0; j<pixels[i].size(); j++){
-            bool chk = checkMarker(j, i);
-            Pixel markerColor(0,0,0);
-            if(chk){
-                cout<<"marker found: "<<j<<"x"<<i<<endl;
+    for(int i = 0; i<width; i++){
+        for(int j = 0; j<height; j++){
+            if(checkMarker(i, j)){
+                cout<<"marker found: "<<i<<"x"<<j<<endl;
             }
         }
     }
