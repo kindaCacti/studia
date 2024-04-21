de8bdd231ce25d6cfecee89e6b179e07a65ecfef
mwawrzy2 <53042410+kindaCacti@users.noreply.github.com>
2024-04-21 16:57:39 +0200

    fixed a bug and made easier to rewrite in asm

--- riscv/proj_risc_v/test.cpp
+++ riscv/proj_risc_v/test.cpp
@@ -5,74 +5,99 @@ using namespace std;
 
 #define width 320
 #define height 240
+#define bytes_per_pixel 3
+#define marker_color 0
 
-//path buffer for file in asm
 string path = "./example_markers.bmp";
-//only one buffer in asm
 char* rde;
 unsigned char* rd;
-//can be saved in register
 int startingPosition;
 
 //instead of pixels use unsigned int int as a pixel
+//(already implemented)
 unsigned int getPixelFromData(int x, int y){
-    int offset = (x + (height - 1 - y) * width) * 3 + startingPosition;
+    int offset = (x + (height - 1 - y) * width) * bytes_per_pixel + startingPosition;
 
     unsigned int pixel = rd[offset + 2];
     pixel <<= 8;
     pixel |= rd[offset + 1];
     pixel <<= 8;
     pixel |= rd[offset];
-    //cout<<pixel<<" : "<<(int)rd[offset + 2]<<", "<<(int)rd[offset + 1]<<", "<<(int)rd[offset + 2]<<endl;
+
     return pixel;
 }
 
-bool checkMarker(int sx, int sy){
-    unsigned int markerColor = 0;
-
-    if(getPixelFromData(sx, sy) != markerColor) return false;
-
+int lineHeight(int sx, int sy){
+    unsigned int current_pixel;
     int hgt = 0;
-    int lhgt = 0;
-    int bhgt = 0;
-    int n = height;
-    int cx = sx;
-
-    bool hasStepDown = false;
-    if(sx-1 >= 0 and getPixelFromData(sx-1, sy) == markerColor) return false;
-    if(sy+1 < height and getPixelFromData(sx, sy-1) == markerColor) return false;
-
-    unsigned int currentPixel;
 
     while(true){
-        hgt = 0;
-        if(cx == width) break; //don't go outside the image
-
-        currentPixel = getPixelFromData(cx, sy);
-
-        if(currentPixel != markerColor) break; //are we still on the marker?
-
-        for(int i = sy; i<n; i++){
-            currentPixel = getPixelFromData(cx, i);
-            if(currentPixel != markerColor) break;
-            hgt++;
-        }
-
-        if(lhgt != 0 and hgt != lhgt){
-            if(hasStepDown) return false;
-            if(hgt > lhgt) return false;
-            hasStepDown = true;
-            bhgt = lhgt;
-        }
-
-        lhgt = hgt;
-        cx++;
+        if(sy >= height) break;
+        current_pixel = getPixelFromData(sx, sy);
+        if(current_pixel != marker_color) break;
+        hgt++;
+        sy++;
     }
-    
-    int wdt = cx - sx;
-    bool out = (((wdt == bhgt/2)|(wdt == (bhgt+1)/2))&hasStepDown);
 
-    return out;
+    return hgt;
+}
+
+bool isLineNotMarker(int sx, int sy, int ey){
+    //outside of the picter is never of a marker type
+    if(sx < 0) return true;
+    if(sy < 0) return true;
+
+    unsigned int current_pixel;
+
+    while(true){
+        if(sy > ey) break;
+        current_pixel = getPixelFromData(sx, sy);
+        if(current_pixel == marker_color) return false;
+        sy++;
+    }
+
+    return true;
+}
+
+bool checkMarker(int sx, int sy){
+    int current_pixel = getPixelFromData(sx, sy);
+
+    if(current_pixel != marker_color) return false;
+
+    int current_x = sx;
+    int current_height = lineHeight(sx, sy);
+    int last_height = current_height;
+    int best_height = current_height;
+    bool hasStep = false;
+    int step_x = -1;
+
+    bool condition = isLineNotMarker(sx, sy-1, sy-1);
+    condition &= isLineNotMarker(sx-1, sy, sy + current_height - 1);
+
+    if(!condition) return false;
+
+    while(true){
+        current_x++;
+        if(current_x >= width) break;
+        current_height = lineHeight(current_x, sy);
+        condition &= isLineNotMarker(current_x, sy-1, sy-1);
+        if(!condition) return false;
+        if(current_height == last_height) continue;
+        if(current_height == 0) break;
+        if(hasStep) return false;
+        hasStep = true;
+        last_height = current_height;
+        step_x = current_x;
+    }
+
+    condition &= hasStep;
+    condition &= isLineNotMarker(step_x, sy + last_height, sy + best_height - 1);
+    condition &= isLineNotMarker(current_x, sy, sy + last_height - 1);
+    int wdt = current_x - sx;
+    condition &= (wdt == best_height / 2);
+
+    if(condition) return true;
+    return false;
 }
 
 int main(){
@@ -94,7 +119,7 @@ int main(){
 
     startingPosition = rd[10];
 
-    getPixelFromData(1, 1);
+    cout<<"pixel: "<<getPixelFromData(1,2)<<endl;
 
     cout<<startingPosition<<endl;
 
